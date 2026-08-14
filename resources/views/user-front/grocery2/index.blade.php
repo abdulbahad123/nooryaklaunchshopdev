84c8d8d7633b6ca80ec4154906798b3252ea0760
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-08-14 18:39:49 +0530

    Set Popular Items slider to 2 columns on mobile view only and update .g2-mid-card to flex-direction column with enlarged image size on mobile view only

--- resources/views/user-front/grocery2/index.blade.php
+++ resources/views/user-front/grocery2/index.blade.php
@@ -450,7 +450,8 @@
         responsive: [
           { breakpoint: 1200, settings: { slidesToShow: 3 } },
           { breakpoint: 992, settings: { slidesToShow: 2 } },
-          { breakpoint: 576, settings: { slidesToShow: 1 } }
+          { breakpoint: 768, settings: { slidesToShow: 2 } },
+          { breakpoint: 576, settings: { slidesToShow: 2 } }
         ],
         rtl: $('html').attr('dir') === 'rtl'
       });
