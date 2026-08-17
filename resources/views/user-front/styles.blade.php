fdb2d2c67d876357a43ea698526117f72bc53c6d
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-08-17 11:08:16 +0530

    File uploade

--- resources/views/user-front/styles.blade.php
+++ resources/views/user-front/styles.blade.php
@@ -658,7 +658,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('assets/user-front/css/grocery2/styles.css?v=1.0.4') }}">
+  <link rel="stylesheet" href="{{ asset('assets/user-front/css/grocery2/styles.css') }}">
 @endif
 <!--====== Style css ======-->
 
