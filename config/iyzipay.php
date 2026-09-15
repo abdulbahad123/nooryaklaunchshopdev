00ffcff78bc32276a766ea5ce9c95910365518b9
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-09-15 18:21:18 +0530

    fix: Prevent duplicate class declaration and return array in config/iyzipay.php

--- config/iyzipay.php
+++ config/iyzipay.php
@@ -2,14 +2,22 @@
 
 namespace Config;
 
-class Iyzipay
-{
-  public static function options($apiKey = null, $secretKey = null, $baseUrl = null)
-  {
-    $options = new \Iyzipay\Options();
-    $options->setApiKey($apiKey ?: config('iyzipay.api_key'));
-    $options->setSecretKey($secretKey ?: config('iyzipay.secret_key'));
-    $options->setBaseUrl($baseUrl ?: config('iyzipay.base_url'));
-    return $options;
-  }
+if (!class_exists('Config\Iyzipay')) {
+    class Iyzipay
+    {
+        public static function options($apiKey = null, $secretKey = null, $baseUrl = null)
+        {
+            $options = new \Iyzipay\Options();
+            $options->setApiKey($apiKey ?: config('iyzipay.api_key'));
+            $options->setSecretKey($secretKey ?: config('iyzipay.secret_key'));
+            $options->setBaseUrl($baseUrl ?: config('iyzipay.base_url'));
+            return $options;
+        }
+    }
 }
+
+return [
+    'api_key'    => env('IYZIPAY_API_KEY', ''),
+    'secret_key' => env('IYZIPAY_SECRET_KEY', ''),
+    'base_url'   => env('IYZIPAY_BASE_URL', 'https://api.iyzipay.com'),
+];
