cdca5ab4ecfaebdbfd1f03172c796a13de98bad4
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-09-24 23:12:11 +0530

    Fix CheckoutRequest validation for Website Builder checkout and payment gateway callbacks

--- app/Http/Requests/Checkout/CheckoutRequest.php
+++ app/Http/Requests/Checkout/CheckoutRequest.php
@@ -23,6 +23,16 @@ class CheckoutRequest extends FormRequest
      */
     public function rules(): array
     {
+        if (isWebsiteBuilderCheckout($this)) {
+            return [
+                'customer_email' => 'nullable|email',
+                'email'          => 'nullable|email',
+                'subdomain'      => 'nullable|max:255',
+                'username'       => 'nullable|max:255',
+                'password'       => 'nullable',
+            ];
+        }
+
         return [
             'first_name' => 'required|max:255',
             // 'last_name' => 'required',
@@ -53,9 +63,54 @@ class CheckoutRequest extends FormRequest
 
     protected function prepareForValidation(): void
     {
-        if ($this->has('phone')) {
+        // 1. If returning from Razorpay or gateway callback, restore session request data
+        if (session()->has('wb_checkout_req')) {
+            $sess = session('wb_checkout_req');
+            if (is_array($sess)) {
+                $this->merge(array_merge($sess, $this->all()));
+            }
+        } elseif (session()->has('data')) {
+            $sess = session('data');
+            if (is_array($sess)) {
+                $this->merge(array_merge($sess, $this->all()));
+            }
+        } elseif (session()->has('request')) {
+            $sess = session('request');
+            if (is_array($sess)) {
+                $this->merge(array_merge($sess, $this->all()));
+            }
+        }
+
+        if (isWebsiteBuilderCheckout($this)) {
+            $cName  = $this->input('customer_name') ?: ($this->input('first_name') ?: ($this->input('shop_name') ?: 'Agency Owner'));
+            $cEmail = $this->input('customer_email') ?: ($this->input('email') ?: '');
+            $cPhone = $this->input('customer_phone') ?: ($this->input('phone') ?: '9360157880');
+            $cSub   = $this->input('subdomain') ?: ($this->input('username') ?: '');
+
+            $cleanPhone = ltrim(preg_replace('/[^0-9]/', '', (string)$cPhone), '0');
+            if (empty($cleanPhone)) $cleanPhone = '9360157880';
+
             $this->merge([
-                'phone' => ltrim(preg_replace('/[^0-9]/', '', $this->phone), '0'),
+                'is_website_builder' => 1,
+                'customer_name'  => $cName,
+                'first_name'      => $this->input('first_name', $cName),
+                'shop_name'       => $this->input('shop_name', $cName),
+                'customer_email' => $cEmail,
+                'email'          => $this->input('email', $cEmail),
+                'customer_phone' => $cPhone,
+                'phone'          => $cleanPhone,
+                'subdomain'      => $cSub,
+                'username'       => $this->input('username', $cSub),
+                'country_code'   => $this->input('country_code', '+91'),
+                'city'           => $this->input('city', 'N/A'),
+                'country'        => $this->input('country', 'India'),
+                'price'          => $this->input('price', 499),
+                'payment_method' => $this->input('payment_method', 'Razorpay'),
+                'password'       => $this->input('password', 'Password@123'),
+            ]);
+        } elseif ($this->has('phone')) {
+            $this->merge([
+                'phone' => ltrim(preg_replace('/[^0-9]/', '', (string)$this->phone), '0'),
             ]);
         }
     }
